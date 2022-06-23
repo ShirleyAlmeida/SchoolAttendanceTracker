9d0975c56d7a315225189eaa099358a3322d8ef7
Shirley <shirleyalmeida9@gmail.com>
2022-06-23 11:50:58 +0530

    Added Bootstrap Dependencies

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,25 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("@popperjs/core")
+import "bootstrap"
+
+import { Tooltip, Popover } from "bootstrap"
+require("../packs/application.scss")
 
 
+document.addEventListener("turbolinks:load", () => {
+    // Both of these are from the Bootstrap 5 docs
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+        return new Tooltip(tooltipTriggerEl)
+    })
+
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function(popoverTriggerEl) {
+        return new Popover(popoverTriggerEl)
+    })
+})
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
